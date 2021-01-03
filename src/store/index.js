27a4cce8122b3ba48f774f68c4c5fd62b1333cec
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-02 23:20:25 -0400

    in this part of the class we created the ShoppingCart and the function to show the informations related to the product price, title, quantity and the total, also we used a currency js file to format the values of the price attribute

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,29 @@ export default new Vuex.Store({
     // = computed properties
     availableProducts(state, getters) {
       return state.products.filter(product => product.inventory > 0);
+    },
+    cartProducts(state) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(
+          product => product.id === cartItem.id
+        );
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity
+        };
+      });
+    },
+    cartTotal(state, getters) {
+      // let total = 0;
+      // getters.cartProducts.forEach(product => {
+      //   total += product.price * product.quantity;
+      // });
+      // return total;
+      return getters.cartProducts.reduce(
+        (total, product) => total + product.price * product.quantity,
+        0
+      );
     }
   },
 
