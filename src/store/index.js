1e229541305388fec8c8c08d51b171f606332695
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-02 22:36:46 -0400

    understanting actions and mutations through examples of use; actions decides when mutations must be fired, mutations work with the state change

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from "vuex";
 import Vue from "vue";
+import shop from "@/api/shop";
 
 Vue.use(Vuex);
 
@@ -18,9 +19,23 @@ export default new Vuex.Store({
 
   actions: {
     // = methods
-    fetchProducts() {
+    fetchProducts({ commit }) {
       // make the call
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit("setProducts", products);
+          resolve();
+        });
+      });
     }
+
+    // addToCart(context, product) {
+    //   if (product.inventory > 0) {
+    //     context.commit("pushProductToCart", product);
+    //   } else {
+    //     // show a out of stock message
+    //   }
+    // }
   },
 
   mutations: {
