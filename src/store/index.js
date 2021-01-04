bab6fd33736f4f1dd0a55177cb05ed6a2cb92cfb
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-03 20:38:20 -0400

    breaking the store file into others seems to make the proccess of using store easier. for example, we can create an action.js file and put all the actions in there, then import the file inside the index.js (store) and set the actions as actions (shorthand);

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import shop from "@/api/shop";
+import actions from "./actions";
 
 Vue.use(Vuex);
 
@@ -48,54 +48,7 @@ export default new Vuex.Store({
     }
   },
 
-  actions: {
-    // = methods
-    fetchProducts({ commit }) {
-      // make the call
-      return new Promise((resolve, reject) => {
-        shop.getProducts(products => {
-          commit("setProducts", products);
-          resolve();
-        });
-      });
-    },
-
-    addProductToCart({ state, getters, commit }, product) {
-      if (getters.productIsInStock(product)) {
-        // find cartItem
-        const cartItem = state.cart.find(item => item.id === product.id);
-        if (!cartItem) {
-          //pushProductToCart
-          commit("pushProductToCart", product.id);
-        } else {
-          //incrementItemQuantity
-          commit("incrementItemQuantity", cartItem);
-        }
-        // temporary remove a product fom the inventory
-        commit("decrementProductInventory", product);
-      }
-    },
-    checkout({ state, commit }) {
-      shop.buyProducts(
-        state.cart,
-        () => {
-          commit("emptyCart");
-          commit("setCheckoutStatus", "success");
-        },
-        () => {
-          commit("setCheckoutStatus", "fail");
-        }
-      );
-    }
-
-    // addToCart(context, product) {
-    //   if (product.inventory > 0) {
-    //     context.commit("pushProductToCart", product);
-    //   } else {
-    //     // show a out of stock message
-    //   }
-    // }
-  },
+  actions,
 
   mutations: {
     // = setting and updating the state
