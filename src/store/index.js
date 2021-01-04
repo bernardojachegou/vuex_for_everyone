90ede66ac3903cfdf4f03cfb355fd07d5e9b1f97
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-03 21:04:36 -0400

    Using vuex Modules is a nice way to keep things organized, we just have to understand how to use it because it uses the rootState to set the global state of the modules;

--- src/store/index.js
+++ src/store/index.js
@@ -1,78 +1,21 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import actions from "./actions";
+import cart from "./modules/cart";
+import products from "./modules/products";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    // = data
-    products: [],
-    // {id, quantity}
-    cart: [],
-    checkoutStatus: null
+  modules: {
+    cart,
+    products
   },
 
-  getters: {
-    // = computed properties
-    availableProducts(state, getters) {
-      return state.products.filter(product => product.inventory > 0);
-    },
-    cartProducts(state) {
-      return state.cart.map(cartItem => {
-        const product = state.products.find(
-          product => product.id === cartItem.id
-        );
-        return {
-          title: product.title,
-          price: product.price,
-          quantity: cartItem.quantity
-        };
-      });
-    },
-    cartTotal(state, getters) {
-      // let total = 0;
-      // getters.cartProducts.forEach(product => {
-      //   total += product.price * product.quantity;
-      // });
-      // return total;
-      return getters.cartProducts.reduce(
-        (total, product) => total + product.price * product.quantity,
-        0
-      );
-    },
-    productIsInStock() {
-      return product => {
-        return product.inventory > 0;
-      };
-    }
-  },
+  state: {},
 
-  actions,
+  getters: {},
 
-  mutations: {
-    // = setting and updating the state
-    setProducts(state, products) {
-      state.products = products;
-    },
+  mutations: {},
 
-    pushProductToCart(state, productId) {
-      state.cart.push({
-        id: productId,
-        quantity: 1
-      });
-    },
-    incrementItemQuantity(state, cartItem) {
-      cartItem.quantity++;
-    },
-    decrementProductInventory(state, cartItem) {
-      cartItem.inventory--;
-    },
-    setCheckoutStatus(state, status) {
-      state.checkoutStatus = status;
-    },
-    emptyCart(state) {
-      state.cart = [];
-    }
-  }
+  actions: {}
 });
